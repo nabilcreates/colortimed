0381ab8146b51542a41747f9ab8e183faf4117f0
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2019-04-21 17:58:40 +0800

    Added mapping function and etc

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import * as $ from 'jquery';
+import p5 from 'p5'
+
+let map = new p5().map
 
 class App extends React.Component{
 
@@ -12,9 +15,17 @@ class App extends React.Component{
 
     convertToHex(time){
         let timestr = time.toISOString().split('T')[1].split('.')[0].split(':')
-        let finaltimestr = timestr[0] + timestr[1] + timestr[2]
+        let hr = Math.round(map(timestr[0], 0 , 24, 0 , 255)).toString(16)
+        let mm = Math.round(map(timestr[1], 0 , 59, 0 , 255)).toString(16)
+        let ss = Math.round(map(timestr[2], 0 , 59, 0 , 255)).toString(16)
 
+        console.log(`hr: ${hr}`)
+        console.log(`mm: ${mm}`)
+        console.log(`ss: ${ss}`)
+        
+        let finaltimestr = `${hr}${mm}${ss}`
         this.setBackgroundColor(finaltimestr)
+
         return finaltimestr
     }
 
@@ -26,6 +37,7 @@ class App extends React.Component{
         return(
             <div id='header'>
                 <h1>#{this.convertToHex(new Date())}</h1>
+                <p>{new Date().toISOString().split('T')[1].split('.')[0]}</p>
                 <h6>Another open-source project by <a href='https://renabil.github.io' >renabil</a></h6>
             </div>
         )
